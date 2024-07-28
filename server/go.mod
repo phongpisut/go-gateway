d1a097abacf64f6f36c49fb8560bd44f836d9905
Phongpisut Meemuk <phongpisut.mee@dplusonline.net>
2024-07-28 18:51:02 +0700

    rename

---
+++ server/go.mod
@@ -0,0 +1,33 @@
+module github.com/phongpisut/go-gateway
+
+go 1.20
+
+require (
+	connectrpc.com/connect v1.16.2
+	github.com/danielgtaylor/huma/v2 v2.19.0
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.21.0
+	github.com/labstack/echo/v4 v4.12.0
+	github.com/soheilhy/cmux v0.1.5
+	go.uber.org/zap v1.27.0
+	google.golang.org/genproto/googleapis/api v0.0.0-20240725223205-93522f1f2a9f
+	google.golang.org/grpc v1.65.0
+	google.golang.org/protobuf v1.34.2
+)
+
+require (
+	github.com/fxamacker/cbor/v2 v2.6.0 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/labstack/gommon v0.4.2 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/crypto v0.24.0 // indirect
+	golang.org/x/net v0.26.0 // indirect
+	golang.org/x/sys v0.21.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240723171418-e6d459c13d2a // indirect
+)
---
+++ server/main.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"context"
+	"crypto/subtle"
+	"fmt"
+	"net"
+	"net/http"
+
+	"github.com/danielgtaylor/huma/v2"
+	"github.com/danielgtaylor/huma/v2/adapters/humaecho"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/soheilhy/cmux"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	_ "github.com/danielgtaylor/huma/v2/formats/cbor"
+	gen "github.com/phongpisut/go-gateway/gen/hello/v1"
+)
+
+// GreetingOutput represents the greeting operation response.
+type GreetingOutput struct {
+	Body struct {
+		Message string `json:"message" example:"Hello, world!" doc:"Greeting message"`
+		Age     int    `json:"age" example:"30" doc:"Age of the person"`
+	}
+}
+
+type GreetingRequest struct {
+	Name string `path:"name" maxLength:"30" example:"world" doc:"Name to greet"`
+	Body *struct {
+		Age int `json:"age,omitempty" maxLength:"3" example:"30" doc:"Age of the person" default:"5"`
+	}
+}
+
+type GreeterServerImpl struct {
+	gen.UnimplementedGreeterServiceServer
+}
+
+func main() {
+	// Create a new router & API
+	logger, _ := zap.NewProduction()
+	defer logger.Sync()
+	sugar := logger.Sugar()
+
+	grpcServer := grpc.NewServer()
+	reflection.Register(grpcServer)
+	gen.RegisterGreeterServiceServer(grpcServer, &GreeterServerImpl{})
+
+	mux := runtime.NewServeMux()
+
+	router := echo.New()
+	router.Use(middleware.Logger())
+
+	config := huma.DefaultConfig("My API", "1.0.0")
+	config.Components.SecuritySchemes = map[string]*huma.SecurityScheme{
+		"bearer": {
+			Type:         "http",
+			Scheme:       "bearer",
+			BearerFormat: "JWT",
+		},
+	}
+	config.DocsPath = ""
+
+	router.GET("/docs", func(c echo.Context) error {
+		return c.HTML(http.StatusOK, `<!doctype html>
+		<html lang="en">
+		  <head>
+			<meta charset="utf-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+			<title>Elements in HTML</title>
+			<!-- Embed elements Elements via Web Component -->
+			<script src="https://unpkg.com/@stoplight/elements/web-components.min.js"></script>
+			<link rel="stylesheet" href="https://unpkg.com/@stoplight/elements/styles.min.css">
+		  </head>
+		  <body>
+		
+			<elements-api
+			  apiDescriptionUrl="/openapi.yaml"
+			  router="hash"
+			  layout="sidebar"
+			/>
+		
+		  </body>
+		</html>`)
+	}, middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
+		// Be careful to use constant time comparison to prevent timing attacks
+		if subtle.ConstantTimeCompare([]byte(username), []byte("admin")) == 1 &&
+			subtle.ConstantTimeCompare([]byte(password), []byte("1234")) == 1 {
+			return true, nil
+		}
+		return false, nil
+	}))
+
+	api := humaecho.New(router, config)
+
+	router.Group("/v1/*{grpc_gateway}").Any("", echo.WrapHandler(mux))
+
+	err := gen.RegisterGreeterServiceHandlerFromEndpoint(context.Background(), mux, "localhost:8888", []grpc.DialOption{grpc.WithInsecure()})
+
+	if err != nil {
+		sugar.Fatal(err)
+	}
+
+	huma.Get(api, "/greeting/{name}", GreetingHandler)
+
+	serverListener, err := net.Listen("tcp", ":8888")
+	if err != nil {
+		sugar.Fatal(err)
+	}
+
+	m := cmux.New(serverListener)
+
+	httpL := m.Match(cmux.HTTP1Fast())
+	grpcL := m.Match(cmux.HTTP2())
+
+	go http.Serve(httpL, router)
+	go grpcServer.Serve(grpcL)
+
+	m.Serve()
+
+}
+
+func GreetingHandler(ctx context.Context, input *GreetingRequest) (*GreetingOutput, error) {
+	resp := &GreetingOutput{}
+	resp.Body.Message = fmt.Sprintf("hello, %s", input.Name)
+	if input.Body != nil && input.Body.Age > 0 {
+		resp.Body.Age = input.Body.Age
+	}
+
+	return resp, nil
+}
+
+func (g *GreeterServerImpl) SayHello(ctx context.Context, request *gen.SayHelloRequest) (*gen.SayHelloResponse, error) {
+	return &gen.SayHelloResponse{
+		Message: fmt.Sprintf("hello %s", request.Name),
+	}, nil
+}
